41e918b9a12a2bcf40f98ec086f439149cdc27df
Pepito <profesorjim@gmail.com>
2020-10-01 10:52:41 +0200

    actualizacion

---
+++ MiPrimerPrograma.java
@@ -0,0 +1,29 @@
+public class MiPrimerPrograma {
+  public static void main (String[] args) {
+    
+    System.out.print("Hola, es mi primer programa.\n\n");
+    System.out.print("La suma de 3 y 5 es: \n");
+    System.out.println(3+5);
+    
+    
+    System.out.print("\033[33m mandarina");
+    System.out.print("\033[32m hierba");
+    System.out.print("\033[31m tomate");
+    System.out.print("\033[37m sábanas");
+    System.out.print("\033[36m cielo");
+    System.out.print("\033[35m nazareno");
+    System.out.print("\033[34m mar");
+    System.out.print("\033[37m "); 
+    
+    System.out.println();
+    System.out.println();
+    
+    System.out.printf("Valor primer entero: %6d - Valor segundo entero: %7.2f\n",3,-58.0);
+    System.out.printf("Para meter un string aquí: %s\n","este es la cadena");
+    System.out.printf("El dato booleano %10b también se puede formatear\n",true);
+    System.out.printf("La barra hacia atrás se escribe: \\ y el tanto por ciento: %%\n");
+    System.out.println("La barra hacia atrás se escribe: \\ y el tanto por ciento: %%\n");
+    
+    
+  }
+}
